46e15f22b3ea31fd550e666213f3ca415d6d00fb
m.moskalenko <m.moskalenko@tbank.ru>
2025-09-05 17:57:13 +0300

    add task 3

--- src/test/java/courseplayw/CardTest.java
+++ src/test/java/courseplayw/CardTest.java
@@ -12,10 +12,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CardTest extends BaseTest {
     private BrowserContext context;
@@ -45,6 +49,16 @@ public class CardTest extends BaseTest {
 
     private Path getTimestampPath(String filename) {
         return Paths.get(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss")) + "/" + filename);
+    }
+
+    @Test
+    void testHomePageVisual() throws IOException {
+        page.navigate("https://the-internet.herokuapp.com");
+        Path actual = Paths.get("actual.png");
+        page.screenshot(new Page.ScreenshotOptions().setPath(actual));
+
+        long mismatch = Files.mismatch(actual, Paths.get("expected.png"));
+        assertThat(mismatch).isEqualTo(-1); // -1 = файлы идентичны
     }
 
     @AfterEach
