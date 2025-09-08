13e8eaebcff249a7cff9a102b80a649aba672fde
m.moskalenko <m.moskalenko@tbank.ru>
2025-09-08 16:16:36 +0300

    add new

--- src/test/java/courseplayw/CardTest.java
+++ src/test/java/courseplayw/CardTest.java
@@ -25,6 +25,7 @@ public class CardTest extends BaseTest {
     private BrowserContext context;
     private Page page;
 
+
     @BeforeEach
     void setup() {
         context = browser.newContext(new Browser.NewContextOptions()
@@ -36,12 +37,12 @@ public class CardTest extends BaseTest {
     void testCartActions() {
         page.navigate("https://the-internet.herokuapp.com/add_remove_elements/");
         // Добавление товара
-        page.click("#add-to-cart");
-        page.locator("#cart").screenshot(new Locator.ScreenshotOptions()
+        page.locator("button[onclick]").filter().click();
+        page.locator("//button[contains(normalize-space(), 'Add Element')]").screenshot(new Locator.ScreenshotOptions()
                 .setPath(getTimestampPath("cart_after_add.png")));
         // Удаление товара
-        page.click("#remove-from-cart");
-        page.locator("#cart").screenshot(new Locator.ScreenshotOptions()
+        page.locator("//button[contains(normalize-space(), 'Delete')]").click();
+        page.locator("//button[contains(normalize-space(), 'Add Element')]").screenshot(new Locator.ScreenshotOptions()
                 .setPath(getTimestampPath("cart_after_remove.png")));
     }
 
@@ -49,15 +50,6 @@ public class CardTest extends BaseTest {
         return Paths.get(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss")) + "/" + filename);
     }
 
-    @Test
-    void testHomePageVisual() throws IOException {
-        page.navigate("https://the-internet.herokuapp.com");
-        Path actual = Paths.get("actual.png");
-        page.screenshot(new Page.ScreenshotOptions().setPath(actual));
-
-        long mismatch = Files.mismatch(actual, Paths.get("expected.png"));
-        assertThat(mismatch).isEqualTo(-1); // -1 = файлы идентичны
-    }
 
     @AfterEach
     void teardown() {
