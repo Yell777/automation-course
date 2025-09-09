7c4df73e8a16aba78d81682df678ff7c169bc41b
m.moskalenko <m.moskalenko@tbank.ru>
2025-09-09 17:58:24 +0300

    add new 3

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,6 +4,8 @@ import com.microsoft.playwright.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.util.Collections;
+
 public class BaseTest {
    public   Playwright playwright;
    public Browser browser;
@@ -16,6 +18,14 @@ public class BaseTest {
         browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
         context = browser.newContext();
         page = context.newPage();
+
+        context.route("**/status_codes/404", route -> {
+            route.fulfill(new Route.FulfillOptions()
+                    .setStatus(200)
+                    .setHeaders(Collections.singletonMap("Content-Type", "text/html"))
+                    .setBody("<h3>Mocked Success Response</h3>")
+            );
+        });
     }
 
 
---
+++ src/test/java/courseplayw/StatusCodeInterceptionTest.java
@@ -0,0 +1,28 @@
+package courseplayw;
+
+import base.BaseTest;
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StatusCodeInterceptionTest extends BaseTest {
+
+    @Test
+    void testMockedStatusCode() {
+        page.navigate("https://the-internet.herokuapp.com/status_codes");
+
+        // Клик по ссылке "404"
+        page.click("//a[contains(normalize-space(), '404')]");
+
+        // Проверка, что отображается мокированный текст
+        String responseText = page.textContent("h3");
+        assertEquals("Mocked Success Response", responseText.trim());
+    }
+
+}
+
